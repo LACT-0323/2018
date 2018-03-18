b7136d4eb5131310ee5e71a6805d5c604c0a0e24
Justin Yu <justin.v.yu@gmail.com>
2018-03-17 19:29:42 -0700

    Add gyro code

--- wallaby/src/Controller.c
+++ wallaby/src/Controller.c
@@ -1,3 +1,4 @@
+
 //
 //  Controller.c
 //  Source written by Justin Yu
@@ -17,7 +18,7 @@ static void right(int angle, float radius, int speed);
  * @return    the parameter `cm` in BackEMF ticks
  */
 long CMtoBEMF(float cm) {
- 	return (long)(cm * 1150. / (M_PI * controller.wheel_diameter));
+    return (long)(cm * 1150. / (M_PI * controller.wheel_diameter));
 }
 
 /**
@@ -26,36 +27,36 @@ long CMtoBEMF(float cm) {
  * @return       the parameter `ticks` in centimeters
  */
 float BEMFtoCM(long ticks) {
- 	return (float)(ticks * (M_PI * controller.wheel_diameter) / 1100.);
+    return (float)(ticks * (M_PI * controller.wheel_diameter) / 1100.);
 }
 
 static void forward(float dist, int speed) {
     if(dist < 0.) {
-    	backward(-dist, speed);
-      	return;
+        backward(-dist, speed);
+        return;
     }
 
-  	// Calculate the # of ticks the robot must move for each wheel
-	long ticks = CMtoBEMF(dist);
-	long totalLeftTicks = gmpc(controller.motor_left) + ticks;
-	long totalRightTicks = gmpc(controller.motor_right) + ticks;
+    // Calculate the # of ticks the robot must move for each wheel
+    long ticks = CMtoBEMF(dist);
+    long totalLeftTicks = gmpc(controller.motor_left) + ticks;
+    long totalRightTicks = gmpc(controller.motor_right) + ticks;
 
-  	// Start motors
-	mav(controller.motor_left, speed);
-	mav(controller.motor_right, speed);
+    // Start motors
+    mav(controller.motor_left, speed * LEFT_MOTOR_CORRECTION);
+    mav(controller.motor_right, speed * RIGHT_MOTOR_CORRECTION);
 
-  	// Keep moving until both motors reach their desired # of ticks
+    // Keep moving until both motors reach their desired # of ticks
     int sum = 0;
-	while(gmpc(controller.motor_left) < totalLeftTicks
+    while(gmpc(controller.motor_left) < totalLeftTicks
           && gmpc(controller.motor_right) < totalRightTicks) {
         sum += gyro_x();
         printf("%d\n", gyro_x());
-		if (gmpc(controller.motor_left) >= totalLeftTicks || gmpc(controller.motor_right) >= totalRightTicks)
-			break;
-	}
+        if (gmpc(controller.motor_left) >= totalLeftTicks || gmpc(controller.motor_right) >= totalRightTicks)
+            break;
+    }
 
-	off(controller.motor_left);
-  	off(controller.motor_right);
+    off(controller.motor_left);
+    off(controller.motor_right);
 }
 
 static void backward(float dist, int speed) {
@@ -64,21 +65,21 @@ static void backward(float dist, int speed) {
         return;
     }
 
-  	// Calculate the # of ticks the robot must move for each wheel
-	long ticks = CMtoBEMF(dist);
-	long totalLeftTicks = gmpc(controller.motor_left) - ticks;
-	long totalRightTicks = gmpc(controller.motor_right) - ticks;
+    // Calculate the # of ticks the robot must move for each wheel
+    long ticks = CMtoBEMF(dist);
+    long totalLeftTicks = gmpc(controller.motor_left) - ticks;
+    long totalRightTicks = gmpc(controller.motor_right) - ticks;
 
-  	// Start motors
-	mav(controller.motor_left, -speed);
-	mav(controller.motor_right, -speed);
+    // Start motors
+    mav(controller.motor_left, -speed);
+    mav(controller.motor_right, -speed);
 
-  	// Keep moving until both motors reach their desired # of ticks
-	while(gmpc(controller.motor_left) > totalLeftTicks
+    // Keep moving until both motors reach their desired # of ticks
+    while(gmpc(controller.motor_left) > totalLeftTicks
           && gmpc(controller.motor_right) > totalRightTicks) {
-		if (gmpc(controller.motor_left) <= totalLeftTicks || gmpc(controller.motor_right) <= totalRightTicks)
-			break;
-	}
+        if (gmpc(controller.motor_left) <= totalLeftTicks || gmpc(controller.motor_right) <= totalRightTicks)
+            break;
+    }
     off(controller.motor_left);
     off(controller.motor_right);
 }
@@ -122,7 +123,7 @@ static void left(int angle, float radius, int speed) {
 
 
 static void right(int angle, float radius, int speed) {
-	// calculate radii
+    // calculate radii
     float left_radius = radius + controller.distance_between_wheels;
     float right_radius = radius;
 
@@ -161,34 +162,34 @@ static void right(int angle, float radius, int speed) {
 // FIX THIS
 static void slow_servo(int port, int position, float time) {
     float increment = .01;
-	float curr, start = controller.get_servo_position(port);
-	float i = ((position - start) / time) * increment;
-	curr = start;
-	if (start > position)
-	{
-		while(curr > position)
-		{
-			controller.servo(port, curr);
-			curr += i;
-			msleep((long)(increment * 1000));
-		}
-	}
-	else if (start < position)
-	{
-		while(curr < position)
-		{
-			controller.servo(port, curr);
-			curr += i;
-			msleep((long)(increment * 1000));
-		}
-	}
-	controller.servo(port, position);
+    float curr, start = controller.get_servo_position(port);
+    float i = ((position - start) / time) * increment;
+    curr = start;
+    if (start > position)
+    {
+        while(curr > position)
+        {
+            controller.servo(port, curr);
+            curr += i;
+            msleep((long)(increment * 1000));
+        }
+    }
+    else if (start < position)
+    {
+        while(curr < position)
+        {
+            controller.servo(port, curr);
+            curr += i;
+            msleep((long)(increment * 1000));
+        }
+    }
+    controller.servo(port, position);
 }
 
 // Constructors
 
 Controller new_controller(int motor_left, int motor_right, float distance_between_wheels, float wheel_diameter) {
-	Controller instance = {
+    Controller instance = {
 
         // Instance Variables (with no setters)
         .distance_between_wheels = distance_between_wheels,
@@ -226,7 +227,7 @@ Controller new_controller(int motor_left, int motor_right, float distance_betwee
 }
 
 Controller new_create_controller() {
-	Controller instance = {
+    Controller instance = {
         // Assign method references
         .motor = &motor,
         .mav = &mav, .mtp = &mtp, .mrp = &mrp,
@@ -249,4 +250,167 @@ Controller new_create_controller() {
     };
     controller = instance;
     return instance;
+}
+
+void line_follow(int cm) {
+    // initialize variables
+
+    int distance = CMtoBEMF(cm);
+    printf("%d\n", distance);
+    int tophat = 0;
+    int et = 1, et_threshold = 1700; // greater than 1600 means that the pole is there
+
+    // PID
+
+    float kp = .8, // proportionality constant .003 //2.4
+    ki = 0.1,  // integral constant .1 //.1
+    kd = 1; // derivative constant 1 //8.2
+    int low, high;
+    float offset = 700; // average of white and black sensor values (660, 985) = 822.5
+    int tp = 1100; // speed of motors at error = 0
+    float integral = 0; // running total of errors
+    float last_error = 0;
+    float derivative = 0; // current error - last error
+
+    int sensor_value, turn;
+    float error;
+    cmpc(controller.motor_left);
+    cmpc(controller.motor_right);
+    while(1) {
+        sensor_value = analog(tophat);
+        error = sensor_value - offset;
+        integral = (7. / 8.) * integral + error;
+        derivative = error - last_error;
+        turn = (int)(kp * error + ki * integral + kd * derivative) / 2;
+        mav(controller.motor_right, tp + turn);
+        mav(controller.motor_left, tp - turn);
+        last_error = error;
+        msleep(1);
+        if(abs(gmpc(controller.motor_left)) > distance || abs(gmpc(controller.motor_right)) > distance)
+            break;
+    }
+}
+
+void forward_gyro(int cm) {
+    // initialize variables
+
+    int distance = CMtoBEMF(cm);
+    printf("%d\n", distance);
+    int tophat = 0;
+    int et = 1, et_threshold = 1700; // greater than 1600 means that the pole is there
+
+    // PID
+
+    float kp = 0.5, // proportionality constant .003 //2.4
+    ki = 0.1,  // integral constant .1 //.1
+    kd = 0.4; // derivative constant 1 //8.2
+    int low, high;
+    int tp = 1100; // speed of motors at error = 0
+    float integral = 0; // running total of errors
+    float last_error = 0;
+    float derivative = 0; // current error - last error
+
+    float avg_straight = 34.5; // average value of gyro_x going straight
+    // if > avg_straight, drifting LEFT
+    // if < avg_straight, drifting RIGHT
+    int turn = 0;
+    int i;
+    float error;
+    cmpc(controller.motor_left);
+    cmpc(controller.motor_right);
+    float avg_x;
+    while(1) {
+        // sensor_value = accel_y();
+        //printf("gyro_x: %d, gyro_y: %d\n", gyro_x(), gyro_y());
+        avg_x = 0;
+        derivative = 0;
+        // take avg
+        float num_samples = 50.;
+        for(i = 0; i < (int)num_samples; i++) {
+            int temp = accel_x();
+            if(abs(temp - 45) < 40 || abs(temp - 29) < 40) {
+            	avg_x += temp;
+            } else {
+                num_samples -= 1;
+            }
+            msleep(1);
+        }
+        avg_x /= num_samples;
+
+        error = avg_x - avg_straight;
+        integral = (5. / 8.) * integral + error;
+        derivative = error - last_error;
+        turn = (int)(kp * error + ki * integral + kd * derivative);
+
+        mav(controller.motor_right, tp - turn * 3);
+        mav(controller.motor_left, tp + turn * 3);
+        last_error = error;
+
+        if(abs(gmpc(controller.motor_left)) > distance || abs(gmpc(controller.motor_right)) > distance) {
+            freeze(controller.motor_left);
+            freeze(controller.motor_right);
+            break;
+        }
+    }
+}
+
+void backward_gyro(int cm) {
+    int distance = CMtoBEMF(cm);
+    printf("%d\n", distance);
+    int tophat = 0;
+    int et = 1, et_threshold = 1700; // greater than 1600 means that the pole is there
+
+    // PID
+
+    float kp = 0.5, // proportionality constant .003 //2.4
+    ki = 0.1,  // integral constant .1 //.1
+    kd = 0.4; // derivative constant 1 //8.2
+    int low, high;
+    int tp = -1100; // speed of motors at error = 0
+    float integral = 0; // running total of errors
+    float last_error = 0;
+    float derivative = 0; // current error - last error
+
+    float avg_straight = 34.5; // average value of gyro_x going straight
+    // if > avg_straight, drifting LEFT
+    // if < avg_straight, drifting RIGHT
+    int turn = 0;
+    int i;
+    float error;
+    cmpc(controller.motor_left);
+    cmpc(controller.motor_right);
+    float avg_x;
+    while(1) {
+        // sensor_value = accel_y();
+        //printf("gyro_x: %d, gyro_y: %d\n", gyro_x(), gyro_y());
+        avg_x = 0;
+        derivative = 0;
+        // take avg
+        float num_samples = 50.;
+        for(i = 0; i < (int)num_samples; i++) {
+            int temp = accel_x();
+            if(abs(temp - 45) < 40 || abs(temp - 29) < 40) {
+            	avg_x += temp;
+            } else {
+                num_samples -= 1;
+            }
+            msleep(1);
+        }
+        avg_x /= num_samples;
+
+        error = avg_x - avg_straight;
+        integral = (5. / 8.) * integral + error;
+        derivative = error - last_error;
+        turn = (int)(kp * error + ki * integral + kd * derivative);
+
+        mav(controller.motor_right, tp + turn * 3);
+        mav(controller.motor_left, tp - turn * 3);
+        last_error = error;
+
+        if(abs(gmpc(controller.motor_left)) > distance || abs(gmpc(controller.motor_right)) > distance) {
+            freeze(controller.motor_left);
+            freeze(controller.motor_right);
+            break;
+        }
+    }
 }
--- wallaby/src/main.c
+++ wallaby/src/main.c
@@ -1,60 +1,54 @@
+
 #include <kipr/botball.h>
 #include "Controller.h"
 
-enum Side {
-    LEFT = 0,
-    RIGHT = 1
-};
+#define CLAW_SERVO 0
+#define CLAW_OPEN 1300
+#define CLAW_VERY_OPEN 850
+#define CLAW_CLOSED 2047
+#define CLAW_MEDIUM 1700
+#define ARM_SERVO 1
+#define ARM_UP 1400
+#define ARM_DOWN 0
 
-void PID(int distance) {
-    // initialize variables
-
-    int tophat = 0;
-    int et = 1, et_threshold = 1700; // greater than 1600 means that the pole is there
-
-    // PID
-
-    float kp = 0.003, // proportionality constant
-    ki = 0.1,  // integral constant
-    kd = 1.0; // derivative constant
-    int low, high;
-    float offset = 0; // average of white and black sensor values (660, 985) = 822.5
-    int tp = 1100; // speed of motors at error = 0
-    float integral = 0; // running total of errors
-    float last_error = 0;
-    float derivative = 0; // current error - last error
-
-    int sensor_value, turn;
-    float error;
-
-    low = 100;
-    high = 3000;
-    offset = (float)(low + high) / 2.;
-    msleep(1000);
-    cmpc(1);
-    while(1) {
-        sensor_value = analog(tophat);
-        error = sensor_value - offset;
-        integral = (7. / 8.) * integral + error;
-        derivative = error - last_error;
-        turn = (int)(kp * error + ki * integral + kd * derivative);
-        mav(1, tp + turn);
-        mav(0, tp - turn);
-        last_error = error;
-        msleep(1);
-        printf("%d\n",gmpc(1));
-        if(gmpc(1)>distance){
-            break;
-        }
-    }
+void open_claw() {
+    controller.servo(CLAW_SERVO, CLAW_OPEN);
+    msleep(100);
 }
 
+void close_claw() {
+    controller.servo(CLAW_SERVO, CLAW_CLOSED);
+    msleep(100);
+}
+/*
+// controller.left (50,0, 1000);
+    // controller.forward (15, 1000);
+    // controller.forward(10, 1200);
+    // controller.right(90, 0, 1200);
+    line_follow(59);
+    open_claw();
+    controller.right(104, 0, 1000);
+    controller.forward(20, 1000);
+    close_claw;
+    */
 int main()
 {
-	controller = new_controller(0, 1, 8, 2);
-    //controller.forward(30, 900);
-    controller.backward(30, 1200);
-    //controller.left(90, 0, 90);
-    // PID(10000);
+	controller = new_controller(0, 1, 7.6, 2);
+    backward_gyro(30);
+    forward_gyro(30);
+    /*
+    controller.servo(CLAW_SERVO, CLAW_MEDIUM);
+    controller.servo(ARM_SERVO, ARM_DOWN);
+    controller.enable_servos();
+    forward_gyro(10);
+    line_follow(44);
+    open_claw();
+    controller.left(109, 0, 1000);
+    controller.forward(19, 1000);
+    line_follow(30);
+    close_claw();
+    controller.backward(50, 1000);
+    controller.servo(CLAW_SERVO, CLAW_VERY_OPEN);
+   */
     return 0;
 }
